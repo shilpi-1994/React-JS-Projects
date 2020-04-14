b4f05e37dbaeb6d5d03c94d6dcdb176d0f6e33b8
shilpi-1994 <shilpisinha380@gmail.com>
2020-04-14 16:16:42 +0530

    Adding modal for order summary when order now button is clicked

---
+++ Burger-Builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Aux from '../../../Hoc/Aux';
+
+const orderSummary = (props) => {
+    const ingredientsSummary = Object.keys(props.ingredients)
+                        .map(igKey => {
+                            return ( <li key={igKey}>
+                                        <span style={{textTransform: 'capitalize'}}>{igKey}: </span>{props.ingredients[igKey]}
+                            </li> );
+                        });
+    return(
+            <Aux>
+            <h3>Your Order</h3>
+            <p>A delicious burger with the following ingredients: </p>
+            <ul>
+                {ingredientsSummary}
+            </ul>
+            <p>Continue to Checkout?</p>
+        </Aux>
+    );
+}
+
+export default orderSummary;
--- Burger-Builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ Burger-Builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Aux from '../../Hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -19,7 +21,8 @@ class BurgerBuilder extends Component {
             cheese: 0
         },
         totalPrice : 4,
-        purchasable: false
+        purchasable: false,
+        purchasing: false
     }
 
     updatePurchaseState (ingredients) {
@@ -64,6 +67,14 @@ class BurgerBuilder extends Component {
         this.updatePurchaseState(updatedIngredients);
     }
 
+    purchaseHandler = () => {
+        this.setState({purchasing: true});
+    }
+
+    purchaseCancelHandler = () => {
+        this.setState({purchasing: false});
+    }
+
     render() {
         const disabledInfo = {
             ...this.state.ingredients
@@ -73,12 +84,16 @@ class BurgerBuilder extends Component {
         }
         return (
             <Aux>
+                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
+                    <OrderSummary ingredients={this.state.ingredients}/>
+                </Modal>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls 
                 ingredientAdded={this.addIngredientHandler} 
                 ingredientRemoved={this.removeIngredientHandler}
                 disabled={disabledInfo}
                 purchasable={this.state.purchasable}
+                ordered={this.purchaseHandler}
                 price={this.state.totalPrice}/>
             </Aux>
         );
