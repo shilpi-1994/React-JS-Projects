f0ff24b7c4394bbe82b7faa6220af5dcca1768aa
shilpi-1994 <shilpisinha380@gmail.com>
2020-04-19 02:58:39 +0530

    Error handling with higher order components

--- Burger-Builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ Burger-Builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,7 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../Hoc/withErrorHandler/withErrorHandler';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -140,4 +141,4 @@ class BurgerBuilder extends Component {
     }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
