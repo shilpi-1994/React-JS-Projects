bcfcf53bb1226e51ebf60ffd7bfd767a3538d105
shilpi-1994 <shilpisinha380@gmail.com>
2020-04-23 12:59:10 +0530

    Article Webiste: Rendering routes and links to switch pages

--- Article-Website/src/containers/Blog/Blog.js
+++ Article-Website/src/containers/Blog/Blog.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
@@ -9,12 +11,17 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to={{
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
-                <Posts/>
+                <Route path="/" exact component={Posts}/>
+                <Route path="/new-post" component={NewPost}/>
             </div>
         );
     }
