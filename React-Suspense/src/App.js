a3391fce6ef68771d7ee35c663e0556f00b4d236
shilpi-1994 <shilpisinha380@gmail.com>
2020-04-27 14:32:23 +0530

    Lazy Loading

--- React-Suspense/src/App.js
+++ React-Suspense/src/App.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense } from 'react';
 import { BrowserRouter, Route, NavLink } from 'react-router-dom';
-
-import Posts from './containers/Posts';
 import User from './containers/User';
 import Welcome from './containers/Welcome';
+
+const Posts = React.lazy(() => {
+  return import('./containers/Posts');
+});
 
 class App extends Component {
   render() {
@@ -16,7 +18,7 @@ class App extends Component {
           </nav>
           <Route path="/" component={Welcome} exact />
           <Route path="/user" component={User} />
-          <Route path="/posts" component={Posts} />
+          <Route path="/posts" render={() => <Suspense fallback={<div>Loading....</div>}><Posts/></Suspense>}/>
         </React.Fragment>
       </BrowserRouter>
     );
