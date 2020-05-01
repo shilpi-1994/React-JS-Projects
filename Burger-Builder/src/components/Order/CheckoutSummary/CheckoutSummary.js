6132fe0c0e71b9edbbf596ec6a5c456f96db5943
shilpi-1994 <shilpisinha380@gmail.com>
2020-05-01 19:58:43 +0530

    Checkout container and routing

---
+++ Burger-Builder/src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Burger from '../../Burger/Burger';
+import Button from '../../UI/Button/Button';
+import classes from './CheckoutSummary.css';
+
+const checkoutSummary = (props) => {
+    return (
+        <div className={classes.CheckoutSummary}>
+            <h1>Hope this burger brings joy to your face.</h1>
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Burger ingredients={props.ingredients}/>
+            </div>
+            <Button btnType="Danger" clicked>CANCEL</Button>
+            <Button btnType="Success" clicked>SUCCESS</Button>
+        </div>
+    );
+
+} 
+
+export default checkoutSummary;
--- Burger-Builder/src/index.js
+++ Burger-Builder/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const app = (
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>
+)
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  app,
   document.getElementById('root')
 );
 
