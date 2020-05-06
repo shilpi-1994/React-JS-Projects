675ab0cb3d577913ff380f6a692a10326dc20e88
shilpi-1994 <shilpisinha380@gmail.com>
2020-05-06 23:57:46 +0530

    Prepping custom dynamic input component

---
+++ Burger-Builder/src/components/UI/Input/Input.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import classes from './Input.css';
+
+const input = (props) => {
+
+    let inputElement = null;
+    switch(props.inputType) {
+        case('input') :
+            inputElement = <input className={classes.InputElement} {...props}/>
+            break;
+        case('textarea') :
+            inputElement = <textarea className={classes.InputElement} {...props}/>
+            break;
+        default: 
+            inputElement = <input className={classes.InputElement} {...props}/>
+    }
+
+    return (
+        <div className={classes.Input}>
+            <label>{props.label}</label>
+            {inputElement}
+        </div>
+    );
+}
+
+export default input;
