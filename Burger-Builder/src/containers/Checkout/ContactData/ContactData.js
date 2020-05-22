b5d17126aef930c4dc12fa0471b3c9ebea00a2da
shilpi-1994 <shilpisinha380@gmail.com>
2020-05-22 14:14:27 +0530

    Handling user input and form submission

--- Burger-Builder/src/containers/Checkout/ContactData/ContactData.js
+++ Burger-Builder/src/containers/Checkout/ContactData/ContactData.js
@@ -66,10 +66,16 @@ class ContactData extends Component {
         e.preventDefault();
         console.log(this.props.ingredients);
 
+        const formData = {};
+        for (let formElementIdentifier in this.state.orderForm) {
+            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier];
+        }
+
         this.setState({loading: true});
         const order = {
             ingredients: this.props.ingredients,
-            price: this.props.price
+            price: this.props.price,
+            orderData: formData
     }
 
     axios.post('/orders.json', order)
@@ -84,6 +90,20 @@ class ContactData extends Component {
             });
     }
 
+    inputChangedHandler = (event, inputIdentifier) => {
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        };
+
+        const updatedFormELement = {
+            ...updatedOrderForm[inputIdentifier]
+        };
+
+        updatedFormELement.value = event.target.value;
+        updatedOrderForm[inputIdentifier] = updatedFormELement;
+        this.setState({orderForm: updatedOrderForm});
+    }
+
     render() {
         const formElementsArray = [];
         for (let key in this.state.orderForm) {
@@ -94,15 +114,16 @@ class ContactData extends Component {
         }
 
         let form = (
-            <form>
+            <form onSubmit={this.orderHandler}>
                 {formElementsArray.map(formElement => (
                     <Input 
                             key={formElement.id}
                             elementType={formElement.config.elementType}
                             elementConfig={formElement.config.elementConfig}
-                            value={formElement.config.value} />
+                            value={formElement.config.value} 
+                            changed={(event) => this.inputChangedHandler(event, formElement.id)} />
                 ))}
-                <Button buttonType="Success" clicked={this.orderHandler}>ORDER</Button>
+                <Button buttonType="Success">ORDER</Button>
             </form>
         );
         if(this.state.loading) {
